cc0b77ba01db4aa5e51353f387782fa9203dd8c0
pritchardn <21726929@student.uwa.edu.au>
2018-12-10 11:39:39 +0800

    Adjusted bash script to set environment variables correctly

--- build/setup_desktop.sh
+++ build/setup_desktop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 source /opt/intel/parallel_studio_xe_2018/psxevars.sh > /dev/null 2>&1
-source /opt/intel/compilers_and_libraries/linux/bin/compilervars.sh intel64
+source /opt/intel/bin/compilervars.sh intel64
 source /opt/intel/mkl/bin/mklvars.sh intel64
-source /opt/intel/compilers_and_libraries_2019.1.144/linux/mpi/intel64/bin/mpivars.sh intel64
+source /opt/intel/compilers_and_libraries/linux/mpi/intel64/bin/mpivars.sh intel64
 
 #make -C ../../graph_sim_fact
