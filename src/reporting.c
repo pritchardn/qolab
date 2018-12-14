b512c4c2e7aac0e583f62d9afce51d47da980994
pritchardn <21726929@student.uwa.edu.au>
2018-12-14 14:24:39 +0800

    + stubs for printing functions

--- src/reporting.c
+++ src/reporting.c
@@ -4,6 +4,28 @@
 
 #include "reporting.h"
 
+
+void machine_report(machine_spec_t * mach_spec){
+
+}
+
+void timing_report(qaoa_statistics_t *statistics){
+
+}
+
+void result_report(qaoa_statistics_t *statistics){
+
+}
+
+void optimiser_report(optimisation_spec_t *opt_spec){
+
+}
+
+void iteration_report(qaoa_data_t *meta_spec){
+
+}
+
+
 void final_report(qaoa_data_t *meta_spec){
     printf("here\n");
 }
