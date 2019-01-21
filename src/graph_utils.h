84e85bde27670aff835b4baddbd93b68d24536b2
pritchardn <21726929@student.uwa.edu.au>
2019-01-21 20:09:50 +0800

    + graph utils included

---
+++ src/graph_utils.h
@@ -0,0 +1,30 @@
+//
+// Created by nicholas on 28/06/18.
+//
+
+#ifndef GRAPHSIMILARITY_GRAPHUTIL_H
+#define GRAPHSIMILARITY_GRAPHUTIL_H
+
+#include <malloc.h>
+#include <mkl.h>
+
+
+void generate_graphs(MKL_INT *graph1, MKL_INT *graph2, int graph_size, float prob);
+
+void copy_graph(const MKL_INT *src, MKL_INT *dest, int graph_size);
+
+void generate_random(MKL_INT *graph, int graph_size, float prob);
+
+void generate_undirected(MKL_INT *graph1, int graph_size, float prob);
+
+void deform_flipped(const MKL_INT *src, MKL_INT *dest, int graph_size);
+
+void deform_add_direct(const MKL_INT *src, MKL_INT *dest, int graph_size, int n);
+
+void deform_add_undirect(const MKL_INT *src, MKL_INT *dest, int graph_size, int n);
+
+void deform_rem_direct(const MKL_INT *src, MKL_INT *dest, int graph_size, int n);
+
+void deform_rem_undirect(const MKL_INT *src, MKL_INT *dest, int graph_size, int n);
+
+#endif //GRAPHSIMILARITY_GRAPHUTILS_H
