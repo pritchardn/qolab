c9a30cc71a2ed21218a58584fd0335e3b667a13d
pritchardn <21726929@student.uwa.edu.au>
2019-02-01 21:23:26 +0800

    Adjusted maximum evaluations based on decomposition amount

--- src/main.c
+++ src/main.c
@@ -12,7 +12,6 @@
  * All parameters which must be specified are in this example.
  */
 int main(int argc, char *argv[]){
-    srand(time(NULL));
     run_spec_t run_spec;
     run_spec.correct = true;
     run_spec.report = false;
@@ -23,16 +22,16 @@ int main(int argc, char *argv[]){
     run_spec.num_samples = 100;
     run_spec.outfile = stdout;
 
-    optimisation_spec_t opt_spec;
-    opt_spec.ftol = 1e-8;
-    opt_spec.xtol = 1e-8;
-    opt_spec.nlopt_method = NLOPT_LN_NELDERMEAD;
-    opt_spec.max_evals = 200 * 1;
-
     machine_spec_t mach_spec;
     mach_spec.num_qubits = 4;
     mach_spec.P = 1;
     mach_spec.space_dimension = (MKL_INT)pow(2, mach_spec.num_qubits);
+
+    optimisation_spec_t opt_spec;
+    opt_spec.ftol = 1e-8;
+    opt_spec.xtol = 1e-8;
+    opt_spec.nlopt_method = NLOPT_LN_NELDERMEAD;
+    opt_spec.max_evals = 200 * mach_spec.P;
 
     cost_data_t cost_data;
     cost_data.cx_range = mach_spec.space_dimension;
