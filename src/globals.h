9af3178e22cc9086d62ec96b733af1bf965b91a6
pritchardn <21726929@student.uwa.edu.au>
2019-02-05 13:51:22 +0800

    Separates best solution and best expectation value

--- src/globals.h
+++ src/globals.h
@@ -24,7 +24,9 @@ typedef struct {
     double result;              // The final (previous) function value
     double classical_exp;       // The classical expectation value of random sampling of the cost function
     double random_exp;          // Randomly sampling the entire QAOA domain (may be different to whole state-space)
+    double best_expectation;    // The best expectation value found
     double best_result;         // The best cost-value found over the entire optimisation scheme
+    double best_result_prob;    // The associated best probability with the best found result
     int max_value, max_index;   // The maximum value and index in the cost function generated
     int term_status;            // The nlopt termination status
     int num_evals;              // The number of evaluations used by the optimiser
