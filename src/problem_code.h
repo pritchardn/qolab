c9de778fcae08171dc8b0476fbe0f4e1ace46b09
pritchardn <21726929@student.uwa.edu.au>
2019-03-19 12:01:30 +0800

    Updates documentation for problem_code.h

--- src/problem_code.h
+++ src/problem_code.h
@@ -8,9 +8,10 @@
 
 #include <mkl.h>
 #include <stdbool.h>
+
 /**
- * A struct which contains additional information for the cost function to work
- * x_range and cx_range must both be included for all cases.
+ * @brief A struct which contains additional information for the cost function to work
+ * @warning x_range and cx_range must both be included for all cases.
  */
 typedef struct {
     MKL_INT x_range;    // The upper bound of cost-function input values.
