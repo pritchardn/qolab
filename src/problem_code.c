3f9012659d7bfd1de28024317f58425e7dee0249
pritchardn <21726929@student.uwa.edu.au>
2019-03-19 09:38:08 +0800

    Updates documentation of problem_code.c

--- src/problem_code.c
+++ src/problem_code.c
@@ -11,6 +11,12 @@ int Cx(int i, int num_qubits, cost_data_t *cost_data){
     return i;
 }
 
+/**
+ * @brief An optional function to be implemented by the user. Determines whether a given candidate solution is valid
+ * @param i The candidate solution
+ * @param cost_data Contains problem dependent data, may or may not be useful
+ * @return True if the given solution is valid, false otherwise
+ */
 bool mask(unsigned int i, cost_data_t *cost_data) {
     return true;
 }
