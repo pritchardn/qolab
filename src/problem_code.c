41784b5320e10d1e236e2c7cfdc3620b49b6f064
pritchardn <21726929@student.uwa.edu.au>
2019-03-19 11:38:22 +0800

    Updates documentation of problem_code.c

--- src/problem_code.c
+++ src/problem_code.c
@@ -5,7 +5,7 @@
 #include "problem_code.h"
 
 /**
- * To be implemented by the user. This defines the problem investigated by the QAOA
+ * @brief To be implemented by the user. This defines the problem investigated by the QAOA
  * @param i
  * @param num_qubits
  * @param cost_data
