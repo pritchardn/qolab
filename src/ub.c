4eaee56cbbb813f2347719892bb1d69e93b83a44
pritchardn <21726929@student.uwa.edu.au>
2019-03-21 15:28:48 +0800

    Updated ub generation

--- src/ub.c
+++ src/ub.c
@@ -14,21 +14,22 @@
  */
 void generate_ub(qaoa_data_t *meta_data, bool (*mask)(unsigned int, cost_data_t *cost_data)) {
     sparse_status_t status;
-    int num_qubits = meta_data->machine_spec->num_qubits;
     MKL_INT nnz = 0;
-    MKL_INT space_dimension = (MKL_INT) pow(2, num_qubits);
-    MKL_Complex16 *values = mkl_calloc((size_t) space_dimension * (num_qubits + 1), sizeof(MKL_Complex16),
+    MKL_INT space_dimension = meta_data->machine_spec->space_dimension;
+    MKL_Complex16 *values = mkl_calloc((size_t) space_dimension * (meta_data->machine_spec->num_qubits + 1),
+                                       sizeof(MKL_Complex16),
                                        DEF_ALIGNMENT);
     MKL_INT *col_begin = mkl_calloc((size_t) space_dimension + 1, sizeof(MKL_INT), DEF_ALIGNMENT);
     MKL_INT *col_end = mkl_calloc((size_t) space_dimension + 1, sizeof(MKL_INT), DEF_ALIGNMENT);
-    MKL_INT *row_index = mkl_calloc((size_t) space_dimension * (num_qubits) + 1, sizeof(MKL_INT), DEF_ALIGNMENT);
+    MKL_INT *row_index = mkl_calloc((size_t) space_dimension * (meta_data->machine_spec->num_qubits) + 1,
+                                    sizeof(MKL_INT), DEF_ALIGNMENT);
     check_alloc(values);
     check_alloc(col_begin);
     check_alloc(col_end);
     check_alloc(row_index);
     for (unsigned int i = 0; i < space_dimension; ++i) {
         col_begin[i] = nnz;
-        for (unsigned int j = 0; j < num_qubits; ++j) {
+        for (unsigned int j = 0; j < meta_data->machine_spec->num_qubits; ++j) {
             unsigned int row = ((i ^ ((unsigned int) 1 << j)));
             if (mask(row, meta_data->cost_data)) {
                 values[nnz].imag = -1.0;
