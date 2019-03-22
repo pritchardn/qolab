22a52df307a05ce3346e4f7119a4a559d0588f8b
pritchardn <21726929@student.uwa.edu.au>
2019-03-22 22:14:07 +0800

    Formatting main

--- src/main.c
+++ src/main.c
@@ -30,16 +30,18 @@ int main(int argc, char *argv[]){
     optimization_spec_t opt_spec;
     opt_spec.ftol = 1e-16;
     opt_spec.xtol = 1e-16;
-    opt_spec.nlopt_method = NLOPT_LN_NELDERMEAD;
     opt_spec.max_evals = 200 * mach_spec.P;
+    opt_spec.nlopt_method = NLOPT_LN_NELDERMEAD;
 
     cost_data_t cost_data;
     cost_data.cx_range = mach_spec.space_dimension;
     cost_data.x_range = mach_spec.space_dimension;
     cost_data.num_vertices = mach_spec.num_qubits;
     cost_data.graph = mkl_malloc(sizeof(MKL_INT) * mach_spec.num_qubits * mach_spec.num_qubits, DEF_ALIGNMENT);
+
     generate_graph(cost_data.graph, mach_spec.num_qubits, 0.5);
     print_graph(&cost_data, stdout);
+
     qaoa(&mach_spec, &cost_data, &opt_spec, &run_spec);
     mkl_free(cost_data.graph);
     return 0;
